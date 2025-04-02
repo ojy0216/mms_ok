5786c7d45366d738ae0617420a56257740f032f5
ojy0216 <ojy7041@gmail.com>
2025-04-02 16:40:00 +0900

    add loguru to requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         "numpy",
         "bitslice",
+        "loguru",
     ],
     author_email="juyoung.oh@snu.ac.kr",
     description="Python package for interfacing with Opal Kelly FPGA boards",
