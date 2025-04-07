6948a7959b00b30c1304a57554dbd3a5057e233a
ojy0216 <ojy7041@gmail.com>
2025-04-07 15:36:16 +0900

    v1.0.0

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="mms_ok",
-    version="0.1.5",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
         "bitslice",
         "loguru",
+        "rich",
     ],
     author_email="juyoung.oh@snu.ac.kr",
     description="Python package for interfacing with Opal Kelly FPGA boards",
