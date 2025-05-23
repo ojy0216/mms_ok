058ce73bf98a409ad02cae53facaa2a19b6f1960
ojy0216 <ojy7041@gmail.com>
2025-05-23 13:25:12 +0900

    v1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mms_ok",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     install_requires=[
         "numpy",
